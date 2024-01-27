5110398398bb11a7ab7f80df723cdb1ce3dd7bda
ekwak <cfcf26@naver.com>
2024-01-27 12:28:50 +0900

    Add initial Django app files

---
+++ 9th/slackbot/app/views.py
@@ -0,0 +1,33 @@
+# views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
+from slack_sdk import WebClient
+import os
+from url_agent import URLAgent
+from langchain_openai import ChatOpenAI
+
+slack_token = os.getenv('SLACK_BOT_TOKEN')
+client = WebClient(token=slack_token)
+
+@csrf_exempt
+@require_POST
+def slack_events(request):
+    json_data = json.loads(request.body)
+    if 'challenge' in json_data:
+        return JsonResponse({'challenge': json_data['challenge']})
+    
+    if json_data['event']['type'] == 'message' and 'subtype' not in json_data['event']:
+        text = json_data['event']['text']
+
+        # ChatOpenAI 인스턴스 생성
+        key = os.getenv('OPENAI_API_KEY')
+        llm = ChatOpenAI(api_key=key, temperature=0)
+
+        # URLAgent 인스턴스 생성
+        URLAgent(llm, text)
+
+        # 추가적인 처리...
+
+    return JsonResponse({'status': 'OK'})
