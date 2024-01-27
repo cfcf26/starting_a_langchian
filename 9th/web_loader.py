7c932ad7092d070e1175441ee49b54cd3c39a819
ekwak <cfcf26@naver.com>
2024-01-27 10:55:50 +0900

    Add save_to_notion function to web_loader.py

--- 9th/web_loader.py
+++ 9th/web_loader.py
@@ -1,5 +1,6 @@
 from web_loader import load_web
 from summarize_documents import summarize
+from save_to_notion import save_to_notion
 from langchain_openai import ChatOpenAI
 
 import os
@@ -10,15 +11,8 @@ key = os.getenv('OPENAI_API_KEY')
 
 llm = ChatOpenAI(api_key=key, temperature=0)
 
-def save_file(url, summaries):
-    with open(f'text.txt', 'w') as f:
-        f.write(f'URL: {url}\n\n')
-        f.write('\nSummaries:\n')
-        summary_text = summaries.get('output_text', '')
-        f.write(f'{summary_text}\n')
-
 if __name__ == '__main__':
-    url = input('url: ')
+    url = 'https://python.langchain.com/docs/integrations/document_loaders/image_captions'
     documents = load_web(url)
     summaries = summarize(llm, documents)
-    save_file(url, summaries)
+    save_to_notion(url, summaries, 'Web')
