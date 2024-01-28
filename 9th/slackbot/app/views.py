91dcda7506e4b8562f1b53903a1217aafe16514e
ekwak <cfcf26@naver.com>
2024-01-28 22:21:31 +0900

    Refactor slack_events function to handle duplicate events

--- 9th/slackbot/app/views.py
+++ 9th/slackbot/app/views.py
@@ -14,6 +14,10 @@ from url_agent import URLAgent
 slack_token = os.getenv('SLACK_BOT_TOKEN')
 client = WebClient(token=slack_token)
 
+# 전역 변수로 처리된 이벤트 ID 저장
+processed_events = set()
+
+
 @csrf_exempt
 @require_POST
 def slack_events(request):
@@ -23,13 +27,19 @@ def slack_events(request):
     # 'challenge' 요청 처리
     if 'challenge' in json_data:
         return JsonResponse({'challenge': json_data['challenge']})
-    else:
-        event = json_data['event']
-        text = event['text']
+    
+    event_id = json_data.get('event_id', '')
+    # 중복된 이벤트인지 확인
+    if event_id in processed_events:
+        return JsonResponse({'status': 'Already processed'})
 
-        # ChatOpenAI 인스턴스 생성 및 URLAgent 처리
-        key = os.getenv('OPENAI_API_KEY')
-        llm = ChatOpenAI(api_key=key, temperature=0)
-        URLAgent(llm, text)
+    processed_events.add(event_id)
+    
+    text = json_data.get('event', {}).get('text', '')
+
+    # ChatOpenAI 인스턴스 생성 및 URLAgent 처리
+    key = os.getenv('OPENAI_API_KEY')
+    llm = ChatOpenAI(api_key=key, temperature=0)
+    URLAgent(llm, text)
 
     return JsonResponse({'status': 'OK'})
