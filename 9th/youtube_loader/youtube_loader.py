d6deb671752050c88f5085b88c9a9c6c3e1b330b
ekwak <cfcf26@naver.com>
2024-01-28 16:50:35 +0900

    Add support for loading YouTube audio in addition to video documents

--- 9th/youtube_loader/youtube_loader.py
+++ 9th/youtube_loader/youtube_loader.py
@@ -1,8 +1,25 @@
 from langchain_community.document_loaders import YoutubeLoader
 
+from langchain_community.document_loaders.blob_loaders.youtube_audio import (
+    YoutubeAudioLoader,
+)
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import OpenAIWhisperParser
+
+def load_youtube_audio(url):
+    urls = [url]
+    save_dir = "~/data/youtube_audio"
+    loader = GenericLoader(YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParser())
+    documents = loader.load()
+    return documents
+
 def load_youtube(url):
     # remove https://www.youtube.com/watch?v=
-    url = url.split("=")[-1]
-    loader = YoutubeLoader(url, language=["ko", "en"])  # 'ko'가 우선, 없을 경우 'en' 사용
+    urls = url.split("=")[-1]
+    loader = YoutubeLoader(urls, language=["ko", "en"])  # 'ko'가 우선, 없을 경우 'en' 사용
     documents = loader.load()
+    if len(documents) == 0:
+        documents = load_youtube_audio(url)
+    if len(documents) == 0:
+        raise Exception("No documents found")
     return documents
