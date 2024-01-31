010783840c36e3bfab58d4ba0d8a6ce73790c4af
ekwak <cfcf26@naver.com>
2024-01-31 22:02:56 +0900

    Add print statement to display loaded documents

--- 9th/web_loader/web_loader.py
+++ 9th/web_loader/web_loader.py
@@ -3,4 +3,6 @@ from langchain_community.document_loaders import WebBaseLoader
 def load_web(url):
     loader = WebBaseLoader(url, autoset_encoding=True)
     documents = loader.load()
+    for doc in documents:
+        print(doc)
     return documents
