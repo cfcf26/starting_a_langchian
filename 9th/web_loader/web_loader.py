6bc261c7da5b7f4b17a12579bec28cfec359ef2b
ekwak <cfcf26@naver.com>
2024-02-04 15:10:54 +0900

    Add SeleniumURLLoader to web_loader.py

--- 9th/web_loader/web_loader.py
+++ 9th/web_loader/web_loader.py
@@ -1,8 +1,14 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import SeleniumURLLoader
 
+# 동적 웹
+def load_web_v2(url):
+    loader = SeleniumURLLoader(url, executable_path="/Users/kwak/starting_a_langchain/9th/web_loader/chromedriver")
+    documents = loader.load()
+    return documents
+
+# 정적 웹
 def load_web(url):
     loader = WebBaseLoader(url, autoset_encoding=True)
     documents = loader.load()
-    for doc in documents:
-        print(doc)
     return documents
