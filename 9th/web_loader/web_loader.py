76d30dd3abc46d696454d64953883ef7ae0c366f
ekwak <cfcf26@naver.com>
2024-02-17 07:43:59 +0900

    Refactor web_loader: Update load_web_v2, add list support Add list support to SeleniumURLLoader in load_web_v2 for dynamic web loading, and rename load_web to load_web_v1 for static web loading. This change enhances flexibility and maintainability. No known issues addressed.

--- 9th/web_loader/web_loader.py
+++ 9th/web_loader/web_loader.py
@@ -3,12 +3,12 @@ from langchain_community.document_loaders import SeleniumURLLoader
 
 # 동적 웹
 def load_web_v2(url):
-    loader = SeleniumURLLoader(url, executable_path="/Users/kwak/starting_a_langchain/9th/web_loader/chromedriver")
+    loader = SeleniumURLLoader([url], executable_path="/Users/kwak/starting_a_langchain/9th/web_loader/chromedriver")
     documents = loader.load()
     return documents
 
 # 정적 웹
-def load_web(url):
+def load_web_v1(url):
     loader = WebBaseLoader(url, autoset_encoding=True)
     documents = loader.load()
     return documents
